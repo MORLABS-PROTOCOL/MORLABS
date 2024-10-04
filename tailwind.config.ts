32b7b96dc1dc48813cc5069eeb8f9a20e9109fdb
DandyJoshua14 <dandyjoshua14@gmail.com>
2024-10-04 10:09:38 +0100

    changed export to module.exports in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = withMT({
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/styles/**/*.css",
   ],
-  theme: {},
+  theme: {
+    extend: {}
+  },
   plugins: [],
-});
+};
 
-export default config;
