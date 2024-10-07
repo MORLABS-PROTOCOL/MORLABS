353921ce07df8b393708235ee9d09c6f50a243a9
genzybas <genzybas@gmail.com>
2024-10-07 12:46:51 +0100

    made modification

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
 import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = {
+const config: Config = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/styles/**/*.css",
   ],
-  theme: {
-    extend: {}
-  },
+  theme: {},
   plugins: [],
-};
+});
 
+export default config;
