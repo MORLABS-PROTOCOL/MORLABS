0141cec2eea21a87d3761e3636a05a4b1f6d09bb
DandyJoshua14 <dandyjoshua14@gmail.com>
2024-10-09 12:28:09 +0100

    fix: trailing slash in cors origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://morlabsprotocol.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://morlabsprotocol.vercel.app'],
 
 }))
 app.get("/", (req, res) => {
